c4c78a89e9145f626b66455a6668fda0d4be3011
DishuiH <39475748+DishuiH@users.noreply.github.com>
2022-04-25 09:47:33 +0800

    first commit

---
+++ common/login.py
@@ -0,0 +1,8 @@
+def login():
+    '''
+    登录系统获取token
+    :return:
+    '''
+    params = {'corpid': 'wwaf0cf3643e92eb30',
+              'corpsecret': 'zE2797_kGEjXq1DAqPocvaDor1kAVg9IEG-EI0c4x30'
+              }
